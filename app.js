e1f79efbf8479a7e655fc811e76456b75c3a3a35
kaan <abbasikaan@gmail.com>
2024-01-10 16:31:28 +0100

    declared functions

--- app.js
+++ app.js
@@ -16,3 +16,15 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 // initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+function goStore() {
+  console.log("Going to store.");
+}
+function goCave() {
+  console.log("Going to cave.");
+}
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
